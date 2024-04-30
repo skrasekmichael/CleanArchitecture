833ec4b8828ee0a39286efb68eb1c1d1874d0a19
Michael Škrášek <skrasek.michal@email.cz>
2024-04-30 15:38:49 +0200

    Get User Teams (#49)
    * add get user teams endpoint, query and query handler
    * add `TeamSlimResponse`
    * add get user teams endpoint tests

---
+++ src/TeamUp.Api/Endpoints/Teams/GetUserTeamsEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TeamUp.Api.Extensions;
+using TeamUp.Application.Teams.GetUserTeams;
+using TeamUp.Contracts.Teams;
+
+namespace TeamUp.Api.Endpoints.Teams;
+
+public sealed class GetUserTeamsEndpoint : IEndpointGroup
+{
+	public void MapEndpoints(RouteGroupBuilder group)
+	{
+		group.MapGet("/", GetTeamAsync)
+			.Produces<List<TeamSlimResponse>>(StatusCodes.Status200OK)
+			.ProducesProblem(StatusCodes.Status401Unauthorized)
+			.ProducesProblem(StatusCodes.Status403Forbidden)
+			.ProducesProblem(StatusCodes.Status404NotFound)
+			.WithName(nameof(GetUserTeamsEndpoint))
+			.MapToApiVersion(1);
+	}
+
+	private async Task<IResult> GetTeamAsync([FromServices] ISender sender, HttpContext httpContext, CancellationToken ct)
+	{
+		var query = new GetUserTeamsQuery(httpContext.GetCurrentUserId());
+		var result = await sender.Send(query, ct);
+		return result.ToResponse(TypedResults.Ok);
+	}
+}
---
+++ src/TeamUp.Application/Teams/GetUserTeams/GetTeamQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using TeamUp.Application.Abstractions;
+using TeamUp.Contracts.Teams;
+
+namespace TeamUp.Application.Teams.GetUserTeams;
+
+internal sealed class GetUserTeamsQueryHandler : IQueryHandler<GetUserTeamsQuery, Result<List<TeamSlimResponse>>>
+{
+	private readonly IAppQueryContext _appQueryContext;
+
+	public GetUserTeamsQueryHandler(IAppQueryContext appQueryContext)
+	{
+		_appQueryContext = appQueryContext;
+	}
+
+	public async Task<Result<List<TeamSlimResponse>>> Handle(GetUserTeamsQuery query, CancellationToken ct)
+	{
+		return await _appQueryContext.Teams
+			.Where(team => team.Members.Any(member => member.UserId == query.InitiatorId))
+			.Select(team => new TeamSlimResponse
+			{
+				TeamId = team.Id,
+				Name = team.Name,
+			})
+			.ToListAsync(ct);
+	}
+}
